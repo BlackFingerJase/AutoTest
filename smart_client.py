707c62713d43a000b0bcc4f5ca8295ed81b2188a
Jase.Kou <57175170+BlackFingerJase@users.noreply.github.com>
2019-11-15 09:48:36 +0800

    add smart server for reference

---
+++ smart_client.py
@@ -0,0 +1,227 @@
+import socket
+import struct
+import json
+import time
+import re
+import os
+
+
+class Client():
+
+    def __init__(self, ip, port_number):
+        self.__ip = ip
+        self.__port_number = port_number
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.connect((self.__ip, self.__port_number))
+
+    def send(self, data):
+        body = json.dumps(data)
+        header = [body.__len__()]
+        headerPack = struct.pack("!I", *header)
+        encode_data = headerPack + body.encode('utf-8')
+        self.__s.send(encode_data)
+        self.__s.send(('').encode('utf-8')) # 发送约定的空字符作为结束标志
+
+    def close(self):
+        self.__s.close()
+
+class SSD():
+    def __init__(self, char_device):
+        self.__char_device = char_device
+        self.disk_num = re.split('/', self.__char_device)[-1]
+        self.__detail = {}
+
+    def __list_to_dict(self, list_info, sep=':'):
+        '''将包含冒号的长字符串转换为字典格式，输入列表，输出字典'''
+        list_temp = []
+        for line in list_info:
+            if line.count(sep) != 1:    # 检查分隔符数量是否为1
+                continue
+            seprate_line = re.split(sep, line)
+            seprate_line = [
+                x.replace('\t', '').replace('\n', '').replace(' ', '')
+                for x in seprate_line
+            ]
+            list_temp.append(seprate_line)
+        dict_info = dict(list_temp)
+        return dict_info
+
+    def __get_pci_bus_num(self):
+        get_pci_cmd = 'find /sys/* -name {0} | grep devices'.format(self.disk_num)
+        hit = os.popen(get_pci_cmd).readlines()
+        if hit:
+            pci_line_str = hit[0]
+            split_hit = re.split('/', pci_line_str)
+            return split_hit[-3]
+
+    def __get_pci_speed(self):
+        pci_bus_num = self.__get_pci_bus_num()
+        get_pci_speed_cmd = "lspci -vvv -s {0} | grep 'LnkSta:' | cut -d ' ' -f 2,4".format(pci_bus_num)
+        lspci_info = os.popen(get_pci_speed_cmd).readlines()
+        lspci_info_strip = [x.strip('\n') for x in lspci_info][0]
+        pci_speed = ''.join(lspci_info_strip.split(','))
+        return pci_speed
+
+    def __get_smart(self):
+        get_smart_cmd = 'nvme smart-log {0}|grep -E "criti|temp|power|unsafe|media|num"'.format(self.__char_device)
+        smart_info = os.popen(get_smart_cmd).readlines()
+        info_dict = self.__list_to_dict(smart_info)
+        return info_dict
+
+    def __get_boot_info(self):
+        get_boot_drive_cmd = "df -h | grep -E '/boot$'"
+        boot_drive_info = os.popen(get_boot_drive_cmd).readlines()[0]
+        key_char = self.__char_device + 'n1'    # 精确匹配，避免nvme1匹配到nvme11.
+        if key_char in boot_drive_info:
+            boot = "Master"
+        else:
+            boot = "Slave"
+        return boot
+
+    def __get_sn_info(self):
+        get_sn_cmd = "nvme id-ctrl {0} | grep -E '^vid|sn|\<mn\>|\<fr\>'".format(self.__char_device)
+        sn_info = os.popen(get_sn_cmd).readlines()
+        info_dict = self.__list_to_dict(sn_info)
+        return info_dict
+
+    def load(self):
+        self.__detail["disk_num"] = self.disk_num
+        self.__detail["boot"] = self.__get_boot_info()
+        self.__detail["pci_num"] = self.__get_pci_bus_num()
+        self.__detail["pci_speed"] = self.__get_pci_speed()
+        smart_dict = self.__get_smart()
+        sn_info_dict = self.__get_sn_info()
+        self.__detail.update(smart_dict)
+        self.__detail.update(sn_info_dict)
+        return
+
+    def dump(self):
+        return self.__detail
+
+class DictCompare():
+    '''模拟一个比较器，用来比较字典的内容'''
+    def __init__(self):
+
+        self.increase = []
+        self.decrease = []
+        self.diff = []
+        
+    def compare(self, new_item, old_item):
+        if not type(old_item) == type(new_item):
+            raise ValueError('type error. different type cannot be compared.')
+        for key in new_item:
+            if key not in old_item:
+                self.increase.append(key)
+            else:
+                if new_item[key] == old_item[key]:
+                    continue
+                else:
+                    self.diff.append(key)
+        for key in old_item:
+            if key not in new_item:
+                self.decrease.append(key)
+        return self.increase, self.decrease, self.diff  
+
+class Commander():
+    '''命令交互框架，内部处理系统打印的返回信息。'''
+    def __init__(self):
+        pass
+    def input(self):
+        '''输入系统命令'''
+        pass
+    def output(self):
+        '''输出系统输出'''
+        pass
+
+def get_char_info():
+    '''获取dera nvme ssd字符设备信息'''
+    get_nvme_node_command = "ls /dev/nvme* | grep nvme.$"
+    node_info_raw = os.popen(get_nvme_node_command).readlines()
+    char_info = [
+        x.replace('\n', '').replace(' ', '').replace('\t', '')
+        for x in node_info_raw if node_info_raw
+    ]
+    return char_info
+
+def compare():
+    pass
+
+def get_current_ssd_info():
+    ssd_info = {}
+    char_info = get_char_info()
+    for char_device in char_info:   # 获取当前ssd信息列表
+        ssd_instanse = SSD(char_device)
+        ssd_instanse.load()
+        single_detail = ssd_instanse.dump()
+        pci_number = single_detail.get('pci_num')
+        ssd_info.update({pci_number : single_detail})
+    return ssd_info
+
+def get_current_server_info():
+    pass
+    return {}
+
+def get_current_script_info():
+    pass
+    return {}
+def get_last_info(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as last_trace:
+            last_info = json.load(last_trace)
+    else:
+        last_info = {}
+    return last_info
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+ 
+    return ip
+
+def pack(identifier, info_type, info_level):
+    
+    info_tobe_send = {"id":identifier, "type":info_type, "level":info_level}
+    if info_level is 2:
+        pass
+    else:
+        info_tobe_send.update({"ssd_info" : current_ssd_info})
+        info_tobe_send.update({"script_info" : current_script_info})
+        info_tobe_send.update({"server_info" : current_server_info})
+        with open('dump.json', 'w') as dump_file:
+            json.dump(info_tobe_send, dump_file)
+    return info_tobe_send
+
+# ========== Western Wall ========== #
+
+differ = DictCompare()
+info_type = "monitor"
+
+while True:
+    last_info = get_last_info('dump.json')
+    current_ssd_info = get_current_ssd_info()
+    current_script_info = get_current_script_info()
+    current_server_info = get_current_server_info()
+
+    if last_info:
+        identifier = last_info['id']
+        last_ssd_info = last_info['ssd_info']
+        last_script_info = last_info['script_info']
+        last_machine_info = last_info['server_info']
+
+        # 需要判断发送什么数据出去，该功能将在下个版本修复。暂时对主程序不构成影响。
+        if last_info != current_ssd_info:
+            info_level = 1
+        else:
+            info_level = 2
+    else:
+        identifier = str(time.time())+ get_host_ip()
+        info_level = 0
+
+    deliver = Client('109.101.80.172', 1025)
+    info_tobe_send = pack(identifier, info_type, info_level)
+    deliver.send(info_tobe_send)
+    time.sleep(60)
---
+++ smart_server.py
@@ -0,0 +1,277 @@
+import socket
+import struct
+import threading
+import time
+import json
+import os
+
+
+class Log():
+    '''log类'''
+    def __init__(self, log_name):
+        if type(log_name) != str:
+            raise TypeError('Wrong log_name type, it should be str.')
+        self.log_name = log_name + '.log'
+
+    def write(self, strings):
+        if type(strings) != str:
+            raise TypeError('Wrong log string type, it should be str.')
+        with open(self.log_name, 'a') as file:
+            c_readable_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            file.write(c_readable_time + '  ' + strings)
+
+    def trash(self):
+        with open(self.log_name, 'w') as file:
+            trash_warning = 'log deleted by host.'
+            file.write(trash_warning)
+
+class Inbox():
+    '''模拟一个支持多线程的socket服务端'''
+
+    def __init__(self, port_number):
+        '''设置socket基础信息'''
+        self.__record = []
+        self.__led = False
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.bind(('', port_number))
+        self.__s.listen(5)
+        self.__s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def on(self):
+        '''将serverDameon函数设置为守护线程，放置在后台运行'''
+        def serverDaemon(self):
+            '''无限循环接受远程联入信息，转交dataRecv函数处理，并为每一次联入单开一个线程，支持多线程'''
+            def dataRecv(self, sock, addr, lock):
+                '''获取锁以后，每1024个字节收一次数据包，解析并添加到类属性列表中，同时设置led状态为True，通讯完毕后端口关闭，释放锁'''
+                lock.acquire()  # 获取进程锁
+                dataBuffer = bytes()    # 初始化网络数据缓存区
+                headerSize = 4  # 规定头信息所占用的字节数
+                while True:    
+                    data = sock.recv(1024)
+                    if data:
+                        dataBuffer += data
+                        if len(dataBuffer) < headerSize:    # 模拟header信息接收不全的状态，
+                            continue
+                        headpack = struct.unpack('!I',dataBuffer[:headerSize])  # header仅包含数据包大小的描述信息
+                        bodySize = headpack[0]
+                        if len(dataBuffer) < headerSize + bodySize: # 模拟单次发送信息不全的情况
+                            continue
+                        body = dataBuffer[headerSize:headerSize+bodySize]
+                        decode_data = json.loads(body.decode('utf-8'))
+                        self.__record.append(decode_data)
+                        self.__led = True
+                        dataBuffer = bytes()    # 考虑到客户端脚本规定，发送完全部信息后关闭端口，dataBuffer无需处理粘包的情况
+                        break
+                    else:
+                        break
+                lock.release()
+                sock.close()
+                return
+            lock = threading.Lock()
+            while True:
+                sock, addr = self.__s.accept()
+                t1 = threading.Thread(target=dataRecv, args=(self, sock, addr, lock))
+                t1.start()
+            return
+        t1 = threading.Thread(target=serverDaemon, args=(self,), daemon=True)
+        t1.start()    
+
+    def led(self):
+        '''判断led灯当前状态'''
+        return self.__led
+
+    def vomit(self):
+        '''返回属性类堆栈中的第一条信息'''
+        if not self.__record:
+            return None
+        else:
+            if len(self.__record) is 1:
+                self.__led = False
+            return self.__record.pop(0)
+
+class DictCompare():
+    '''模拟一个比较器，用来比较字典的内容'''
+    def __init__(self):
+
+        self.increase = []
+        self.decrease = []
+        self.diff = []
+        
+    def compare(self, new_item, old_item):
+        self.increase = []  # bug fix: log打印重复信息
+        self.decrease = []  # bug fix: log打印重复信息
+        self.diff = []      # bug fix: log打印重复信息
+
+        if not type(old_item) == type(new_item):
+            raise ValueError('type error. different type cannot be compared.')
+        if new_item and old_item:
+            for key in new_item:
+                if key not in old_item:
+                    self.increase.append(key)
+                else:
+                    if new_item[key] == old_item[key]:
+                        continue
+                    else:
+                        self.diff.append(key)
+            for key in old_item:
+                if key not in new_item:
+                    self.decrease.append(key)
+        elif new_item:
+            self.increase = list(old_item.keys())
+        elif old_item:
+            self.decrease = list(new_item.keys())
+
+        return self.increase, self.decrease, self.diff    
+
+
+def load_main_dict(main_dict_file):
+    '''尝试从本地json文件恢复main_dict,以防止主控以外关闭导致的数据丢失'''
+
+    if os.path.exists(main_dict_file):
+        with open(main_dict_file, 'r') as main_dict_obj:
+            main_dict = json.load(main_dict_obj)
+    else:
+        main_dict = dict()
+    return main_dict
+
+def process(message):
+    if isinstance(message, dict) and "type" in message:
+        message_type = message.pop("type")
+    else:
+        raise ValueError("message type error, it should be dict and contain 'type' key")
+    
+    if message_type == "monitor":
+        monitor_processor(message)
+    elif message_type == "powercycle":
+        powercycle_processor(message)
+    
+def powercycle_processor(message):
+    print('under construction.')
+    pass
+
+def monitor_processor(message):
+    '''主字典刷新及log记录操作'''
+    identifier = message.pop("id") # id should be timestamp + ip string,
+    info_level = message.pop("level") # 0 init 1 update 2 heart_beat
+    if info_level is 0:
+        sub_init_process(identifier, message)
+    elif info_level is 1:
+        server_info = message.get("server_info")
+        script_info = message.get("script_info")
+        ssd_info = message.get("ssd_info")
+        if server_info:
+            sub_single_dict_process("server_info",server_info,identifier)
+        if script_info:
+            sub_single_dict_process("script_info",script_info,identifier)
+        if ssd_info:
+            sub_ssd_process(ssd_info, identifier)
+    elif info_level is 2:
+        info = '[{0}] ^^^ Alive Detected ^^^'.format(identifier)
+        main_log.write(info)
+    main_dict.update({identifier : message})
+    return
+
+def sub_init_process(identifier, message):
+    # server_info = message.get("server_info")
+    ssd_info = message.get("ssd_info")
+    info = '====== New Clietn Join ======\n'
+
+
+    for key, value in ssd_info.items():
+        info = info + '{0} : {1}\n'.format(key, value)
+    main_log.write(info)
+    warning_log.write(info)
+
+def sub_single_dict_process(single_dict_name, single_dict_info, identifier):
+    old_record = main_dict.get(identifier)
+    if old_record:
+        old_dict_info = old_record.get(single_dict_name)
+        increase_key, decrease_key, diff_key = dict_comp.compare(single_dict_info, old_dict_info)
+        if increase_key:
+            for key in increase_key:
+                info = '[{0}] +++ Add Detected +++ [add {1} : {2}]\n'.format(identifier, key, single_dict_info[key])
+                main_log.write(info)
+                warning_log.write(info)
+        if decrease_key:
+            for key in decrease_key:
+                info = '[{0}] xxx Remove Detected xxx [remove {1} : {2}]\n'.format(identifier, key, single_dict_info[key])
+                main_log.write(info)
+                warning_log.write(info)
+                error_log.write(info)
+        if diff_key:
+            for key in diff_key:
+                info = '[{0}] ??? Change Detected ??? [change {1} : {2} ===> {3}\n]'.format(identifier,key,old_dict_info[key],single_dict_info[key])
+    else:
+        print('{0} info not found, init client to resend the message.'.format(identifier))
+
+    return
+
+def sub_ssd_process(new_ssd_sum, identifier):
+    old_ssd_sum = main_dict[identifier]["ssd_info"]
+    insert_ssd, eject_ssd, diff_ssd = dict_comp.compare(new_ssd_sum, old_ssd_sum)
+    if insert_ssd:
+        for bus_num in insert_ssd:
+            info = '{0} +++ SSD Insert Detected +++\n'.format(identifier)
+            ssd_detail = new_ssd_sum[bus_num]
+            for key, value in ssd_detail.items():
+                info += '[{0} : {1}]\n'.format(key, value)
+            print(info)
+            main_log.write(info)
+    if eject_ssd:
+        for bus_num in eject_ssd:
+            info = '{0} xxx SSD Eject Detected xxx\n'.format(identifier)
+            ssd_detail = old_ssd_sum[bus_num]
+            for key, value in ssd_detail.items():
+                info += '[{0} : {1}]\n'.format(key, value)
+            print(info)
+            main_log.write(info)
+    if diff_ssd:
+        head = '{0} --- SSD Info Change Notice ---\n'.format(identifier)
+        bodybox = []
+        for bus_num in diff_ssd:
+            new_ssd_detail = new_ssd_sum[bus_num]
+            old_ssd_detail = old_ssd_sum[bus_num]
+            disk_num = new_ssd_detail['disk_num']
+            boot = new_ssd_detail['boot']
+            pci_num = new_ssd_detail['pci_num']
+            sn_num = new_ssd_detail['sn']
+            body = '[{0}][{1}][{2}]-{3}-'.format(pci_num,sn_num,disk_num,boot)
+            for item in new_ssd_detail:
+                if new_ssd_detail[item] != old_ssd_detail[item]:
+                    if item == 'temperature':
+                        if int(new_ssd_detail[item][:-1]) < 60: 
+                            continue
+                    info = '  [{0} : {1} ===> {2}]\n'.format(item, old_ssd_detail[item], new_ssd_detail[item])
+                    notice = body + info
+                    bodybox.append(notice)
+        if bodybox:
+            print(head)
+            main_log.write(head)
+            for notice in bodybox:
+                print(notice)
+                main_log.write(notice)
+        else:
+            info = '[{0}] ^^^ Alive Detected ^^^\n'.format(identifier)
+            print(info)
+            main_log.write(info)
+
+
+# ------ Danger Zone ------ #
+main_dict_file = 'main_dict.json'
+monitor_inbox = Inbox(1025) # 建立监控服务器
+dict_comp = DictCompare()
+main_log = Log('main')  # 定义主log
+warning_log = Log('warning')    # 定义警告log
+error_log = Log('error')    # 定义错误log
+
+monitor_inbox.on()  # 开启监控进程
+main_dict = load_main_dict(main_dict_file)  # 读取主数据库
+
+while True:
+    if monitor_inbox.led():
+        message = monitor_inbox.vomit()
+        process(message)
+    else:
+        with open(main_dict_file, 'w') as db:
+            json.dump(main_dict, db)
+        time.sleep(2)
