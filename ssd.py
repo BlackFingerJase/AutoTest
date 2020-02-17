e7bcbe61d77b24398fb98120d61be0d3587fd2c9
Jase.Kou <57175170+BlackFingerJase@users.noreply.github.com>
2020-02-17 17:17:01 +0800

    Create ssd.py
    创建ssd结构体，包含smart，secure erase，等基础功能

---
+++ ssd.py
@@ -0,0 +1,18 @@
+import os
+
+class ssd_structure
+
+    def __init__(self, nvme_name):
+        self.__smart_info = {}
+        self.__list_inf0 = {}
+        self.report = {}
+        
+    def check_stat(self):
+        pass
+        
+    def secure_erase(self):
+        pass
+        
+    def precondition(self):
+        pass
+        
