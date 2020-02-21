d44622ddb673993d441143d0e71bb4c05494ce55
Jase.KouBizLapTop <57175170+BlackFingerJase@users.noreply.github.com>
2020-02-21 09:26:03 +0800

    add ssd structure function frams

--- basic_lib.py
+++ basic_lib.py
@@ -1,13 +1,14 @@
 import os
 import re
 
+
 def command(cmd):
     raw_output = os.popen(cmd).readlines()
     matrix = []
-    
+
     for line in raw_output:
         line = line.strip()
-        output = re.split('[\/,;\s]', line)
+        output = re.split("[\/,;\s]", line)
         matrix.append(output)
-        
+
     return matrix
--- ssd.py
+++ ssd.py
@@ -1,18 +1,36 @@
-import os
 
-class ssd_structure
 
+class ssd_structure(object):
     def __init__(self, nvme_name):
         self.__smart_info = {}
         self.__list_inf0 = {}
         self.report = {}
-        
+
     def check_stat(self):
         pass
-        
+
     def secure_erase(self):
         pass
-        
+
     def precondition(self):
         pass
-        
+
+
+def get_bus_num(nvme_name):
+    pass
+    return
+
+
+def get_smart(nvme_name):
+    pass
+    return
+
+
+def get_smart_extend(nvme_name):
+    pass
+    return
+
+
+def get_basic(nvme_name):
+    pass
+    return
